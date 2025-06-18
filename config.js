58617ec6d14aaa6ab7910fd0d7563891621f3bf4
Chase Lean <chaseleanrocks@gmail.com>
2025-06-18 20:36:52 +0200

    Updates backend URL

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
 export const API_CONFIG = {
     // BASE_URL: 'http://localhost:8000/quotes',
-    BASE_URL: 'https://quoteverse-backend-wpus5.ondigitalocean.app/quotes',
+    // BASE_URL: 'https://quoteverse-backend-wpus5.ondigitalocean.app/quotes',
+    BASE_URL: 'https://quoteverse-backend.onrender.com/quotes',
     MAX_QUOTE_COUNT: 1000,
     MAX_SIMILAR_QUOTE_COUNT: 50,
     MAX_SIMILAR_QUOTE_DISTANCE: 0.55,
     REQUEST_INTERVAL: 50,
     REQUEST_CHECK_INTERVAL: 1000,
     RANDOMIZE_QUOTES: true,
-    AUTO_SEND: false
+    AUTO_SEND: true
 };
 
 export const MESSAGES = {
